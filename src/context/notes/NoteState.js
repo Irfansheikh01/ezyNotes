94c79e1f2b4b3a78cc628cd26d8e59cd9fbb8453
Irfan <irfansheikh41297@gmail.com>
2024-06-14 08:29:51 +0530

    real host was commented

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,8 +2,8 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  // const host = "https://mern-api-backend-rho.vercel.app"
-  const host = "http://localhost:5000"
+  const host = "https://mern-api-backend-rho.vercel.app"
+  // const host = "http://localhost:5000"
   const [notes, setNotes] = useState([])
   const [user,setUser] = useState([])
 
